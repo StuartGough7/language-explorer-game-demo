b60b0d231a17d7bb65b13a3147d541aecf670579
Stuart Gough <stuartgough7@gmail.com>
2019-11-03 21:30:38 +0000

    Created Basic Tapcontroller

---
+++ Language Explorer Demo/Assets/Scripts/TapController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))] // This will create a RigidBody2D component on anything the script is made a part of (almost like Typescript static types)
+public class TapController : MonoBehaviour
+{
+    public float tapForce = 200;
+    public float tiltSmooth = 5;
+    public Vector3 starPos;
+
+    private Rigidbody2D rigidBody; // you dont need to explicitly say private
+    Quaternion downRotation;
+    Quaternion forwardRotation;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler(0, 0, -90);
+        forwardRotation = Quaternion.Euler(0, 0, 35);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.rotation = forwardRotation;
+            rigidBody.velocity = Vector2.zero; // If you dont add this the tap has to fight against the current velocity and wont do enough. This makes the velocity 0 and the bounce more snappy
+            rigidBody.AddForce(Vector2.up * tapForce, ForceMode2D.Force); // The second argument of Force mode has an impulse option as well. Force is the default and is optional
+        }
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "ScoreZone")
+        {
+            // Increment Score
+        }     
+        if(collision.tag == "DeadZone")
+        {
+            rigidBody.simulated = false; // freeze the character
+
+            // register dead event
+            // game over
+        }
+    }
+
+}
