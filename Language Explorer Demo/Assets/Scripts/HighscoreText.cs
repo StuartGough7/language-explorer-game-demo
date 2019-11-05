35f99e956e2fefb0a7e49031c8c97e292dfc21ec
Stuart Gough <stuartgough7@gmail.com>
2019-11-05 21:14:01 +0000

    Highscore text script

---
+++ Language Explorer Demo/Assets/Scripts/HighscoreText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class HighscoreText : MonoBehaviour
+{
+    Text highScore;
+
+    private void OnEnable() // we use this instead of start beacus we need to re check the high score every time the screen shows not just on mount
+    {
+        highScore = GetComponent<Text>();
+        highScore.text = PlayerPrefs.GetInt("HighScore").ToString();
+        
+    }
+
+}
